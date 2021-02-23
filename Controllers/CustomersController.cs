87da2477037762b05027f520e45ff50eda8ccd0a
gk19989 <maheshgudumala13@gmail.com>
2021-02-23 13:38:09 +0530

    Adding Validation

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -31,11 +31,20 @@ namespace Vidly.Controllers
                 MembershipTypes = membershipTyps
             };
 
-            return View("CustomerForm");
+            return View("CustomerForm", viewModel);
         }
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if(customer.Id == 0)
             {
                 _context.Customers.Add(customer);
