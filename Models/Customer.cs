f9f92b64dd32ed0e115ecb98f6524077e76ced66
gk19989 <maheshgudumala13@gmail.com>
2021-02-23 14:17:59 +0530

    Custom Validation

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -19,6 +19,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
         
         [Display(Name="Date of Birth")]
+        [Min18YearsIfAmember]
         public DateTime? BirthDay { get; set; }
     }
 }
