0e91eef2f6f325bae493c434b35eeaf9749f5c81
gk19989 <maheshgudumala13@gmail.com>
2021-02-28 23:27:39 +0530

    Returning Hierarchial Data

--- Dtos/CustomerDto.cs
+++ Dtos/CustomerDto.cs
@@ -17,7 +17,7 @@ namespace Vidly.Dtos
 
         public bool IsSubscribedToNewsLetter { get; set; }
 
-       
+        public MembershipTypeDto MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
 
         
