0b29ffc8fa464b743761adc82e888646a6a268e8
gk19989 <maheshgudumala13@gmail.com>
2021-03-02 14:49:34 +0530

    Glimpse and Output Cache

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Vidly.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration =50)]
         public ActionResult Index()
         {
             return View();
